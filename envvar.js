29bddd5417cb338adce1311bb6b99d0858458f25
Pat  Pignataro <pat6284@msn.com>
2019-06-29 13:05:03 +0000

    Automatically backed up by Learn

--- envvar.js
+++ envvar.js
@@ -1,6 +1,14 @@
-function envvar() {
-  process.env.NODE_ENV = 'qa'
-  return process.env.NODE_ENV
-}
+// function envvar() {
+//   process.env.NODE_ENV = 'qa'
+//   return process.env.NODE_ENV
+// }
 
- module.exports = envvar
+// module.exports = envvar
+ 
+ const process = require('process');
+
+ const env = () => {
+  return process.env.NODE_ENV;
+};
+
+ module.exports = env;
